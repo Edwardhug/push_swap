70cce59856a992543e2f06e23ba2f628aeb7d431
root <root@DESKTOP-7CO9QQK.localdomain>
2023-02-01 16:54:24 +0100

    starting to do the tab

--- src/ft_make_tab.c
+++ src/ft_make_tab.c
@@ -1,26 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_make_tab.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lgabet <lgabet@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/31 19:13:06 by lgabet            #+#    #+#             */
-/*   Updated: 2023/02/01 16:52:45 by lgabet           ###   ########.fr       */
+/*   Created: 2023/02/01 16:46:09 by lgabet            #+#    #+#             */
+/*   Updated: 2023/02/01 16:53:45 by lgabet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-int	main(int ac, char **av)
+int	*ft_fill_tab(int ac, char **av)
 {
 	int	*tab;
+	int	i;
 
-	if (have_error(ac, av) == 1)
-	{
-		write(2, "Error\n", 6);
-		return (1);
-	}
-	tab = ft_fill_tab(ac, av);
-	return (0);
+	tab = malloc(sizeof(int) * (ac - 1));
+	if (!tab)
+		return (NULL);
 }
